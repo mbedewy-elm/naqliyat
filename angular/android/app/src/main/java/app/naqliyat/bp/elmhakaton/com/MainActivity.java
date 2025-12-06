c01848827baa488b235ab112f7e0d5fb4be2e480
Muhammad Albedewy <albedewy@outlook.com>
2025-12-06 11:25:47 +0300

    add android and ios apps

---
+++ angular/android/app/src/main/java/app/naqliyat/bp/elmhakaton/com/MainActivity.java
@@ -0,0 +1,5 @@
+package app.naqliyat.bp.elmhakaton.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ angular/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.naqliyat.bp.elmhakaton.com',
+  appName: 'NaqliyatApp',
+  webDir: 'dist\Naqliyat\browser'
+};
+
+export default config;
